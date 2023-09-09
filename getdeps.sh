9d6634196f87b469b5b168f2e40e9517feb52f08
GoldenStarGamer <vicentetavareslima@gmail.com>
2023-09-09 12:20:35 +0100

    made the dependency install script more clear

--- getdeps.sh
+++ getdeps.sh
@@ -1,1 +1,8 @@
+if ! command -v apt &>/dev/null; then
+  echo "Error: 'apt' package manager not found on this Linux distribution. This script only supports distros with apt."
+  echo "You will have to download the dependencies manually."
+  exit 1
+fi
+
+sudo apt update
 sudo xargs apt install < DEPENDENCIES.txt
